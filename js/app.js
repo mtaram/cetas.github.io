a2861eb0774b7befd2a7c229a1e5e999860c5c3f
thedewer <harsh.patel54@gmail.com>
2016-07-26 03:16:50 +0530

    New Site update
    New Site update

--- js/app.js
+++ js/app.js
@@ -56,7 +56,7 @@ particlesJS("particles-js", {
     },
     "move": {
       "enable": true,
-      "speed": 1,
+      "speed": 2,
       "direction": "none",
       "random": true,
       "straight": false,
